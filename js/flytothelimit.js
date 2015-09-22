5f2d79a6ddeb5b03c36054677e72b27931f8f39b
unknown <shaoting.wu@KENT-103-16.wellington.natcoll.ac.nz>
2015-09-22 15:27:17 +1200

    Signed-off-by: unknown <shaoting.wu@KENT-103-16.wellington.natcoll.ac.nz>

--- js/flytothelimit.js
+++ js/flytothelimit.js
@@ -1,7 +1,33 @@
-$(window).scroll(function() {
-				if ($(this).scrollTop() > 50) {
-                    $('nav').addClass('fix');
-		        } else {
-		            $('nav').removeClass('fix');
-		        }
-		});
+// $(window).scroll(function() {
+// 				if ($(this).scrollTop() > 50) {
+//                     $('nav').addClass('fix');
+// 		        } else {
+// 		            $('nav').removeClass('fix');
+// 		        }
+// 		});
+
+document.addEventListener('DOMContentLoaded', function()
+{
+	var theForm = document.querySelector('#contact_form');
+	console.log(theForm);
+
+	theForm.noValidate = ture;
+
+    theForm.addEventListener('Submit',function(evt)
+    {
+    	var isError = false;
+    	var elements = this.elements;
+    	for(var i = 0; i < elements[i]; i+=1)
+    	{
+    		var field = elements[i];
+    		if(field.value.trim() === ""){
+    			isError = true; ????????????
+    			field.classList
+    		}
+    	}
+    	if (isError)
+    	{
+    		evt.preventDefault();
+    	}
+    });
+});
